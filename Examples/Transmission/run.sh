9178aad79208b8d9bd4c81658dc0691bcfbb1915
Annemarie Theulings <a.theulings@nikhef.nl>
2016-06-29 13:41:12 +0200

    Transmission example

---
+++ Examples/Transmission/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+job=results
+for material in Alumina Silicon
+do
+	echo Starting job for ${material} 
+	sed "s/MATERIAL/${material}/g" YieldCurve.mac > Yield${material}.mac
+	./SEM YieldCurve.gdml Yield${material}.mac > Yield${material}.log 2>&1
+	echo Done with job ${job} for ${material}
+done
