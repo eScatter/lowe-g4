e55249aa6a5f1bf0fdc29fc7c20ea45dd5c13b3e
Annemarie Theulings <a.theulings@nikhef.nl>
2016-08-19 12:51:17 +0200

    resolved runtime error: could not find libCADPhysics.so

--- Examples/Transmission/run.sh
+++ Examples/Transmission/run.sh
@@ -2,8 +2,8 @@
 job=results
 for material in Alumina Silicon
 do
-	echo Starting job for ${material} 
+	echo Starting job for ${material}
 	sed "s/MATERIAL/${material}/g" YieldCurve.mac > Yield${material}.mac
-	./SEM YieldCurve.gdml Yield${material}.mac > Yield${material}.log 2>&1
+	LD_PRELOAD=/usr/local/lib/libCADPhysics.so ./SEM YieldCurve.gdml Yield${material}.mac > Yield${material}.log 2>&1
 	echo Done with job ${job} for ${material}
 done
