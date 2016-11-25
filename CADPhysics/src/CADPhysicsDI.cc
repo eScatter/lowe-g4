a5509b27b2031366eea9b06ef9023bf6b0eaa705
Annemarie Theulings <a.theulings@nikhef.nl>
2016-11-25 12:57:39 +0100

    Improved potential barrier calculation for insulators and semiconductors

--- CADPhysics/src/CADPhysicsDI.cc
+++ CADPhysics/src/CADPhysicsDI.cc
@@ -243,7 +243,11 @@ void CADPhysicsDI::BuildPhysicsTable(const G4ParticleDefinition& /*aParticleType
          vec_minimumlimit.push_back(minlimit);
          vec_bandgap.push_back(bandgap);
          vec_conductortype.push_back(2);
-         barrier = fermieff + work - bb;
+         if(fermieff) {
+           barrier = fermieff + work + bandgap/2.0 - bb;
+         } else {
+           barrier = work + bandgap - bb;
+         }
          vec_barrier.push_back(barrier);
       } else if (cond==1) {// Semiconductor
 
@@ -254,7 +258,11 @@ void CADPhysicsDI::BuildPhysicsTable(const G4ParticleDefinition& /*aParticleType
          vec_minimumlimit.push_back(minlimit);
          vec_bandgap.push_back(bandgap);
          vec_conductortype.push_back(1);
-         barrier = fermieff + work - bb;
+         if(fermieff) {
+           barrier = fermieff + work + bandgap/2.0 - bb;
+         } else {
+           barrier = work + bandgap - bb;
+         }
          vec_barrier.push_back(barrier);
       } else {// Metal
          vec_fermieff.push_back(fermieff);
