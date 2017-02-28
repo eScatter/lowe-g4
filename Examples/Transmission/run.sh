4561957f5c3998caa35164b7010f1c4668ecf7d8
Annemarie Theulings <ammg.theulings@salteh.net>
2017-02-28 13:27:08 +0100

    Update run.sh

--- Examples/Transmission/run.sh
+++ Examples/Transmission/run.sh
@@ -4,6 +4,6 @@ for material in Alumina Silicon
 do
 	echo Starting job for ${material}
 	sed "s/MATERIAL/${material}/g" YieldCurve.mac > Yield${material}.mac
-	LD_PRELOAD=/usr/local/lib/libCADPhysics.so ./SEM YieldCurve.gdml Yield${material}.mac > Yield${material}.log 2>&1
+	LD_PRELOAD=/usr/local/lib/libCADPhysics.so SEM YieldCurve.gdml Yield${material}.mac > Yield${material}.log 2>&1
 	echo Done with job ${job} for ${material}
 done
