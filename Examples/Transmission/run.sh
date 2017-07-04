002e610da60a98f0ab7703ebf870562f783530ac
Luc van Kessel <L.C.P.M.vanKessel@tudelft.nl>
2017-07-04 17:04:57 +0200

    Remove LD_PRELOAD in run.sh.
    Because we don't need it.

--- Examples/Transmission/run.sh
+++ Examples/Transmission/run.sh
@@ -4,6 +4,6 @@ for material in Alumina Silicon
 do
 	echo Starting job for ${material}
 	sed "s/MATERIAL/${material}/g" YieldCurve.mac > Yield${material}.mac
-	LD_PRELOAD="/usr/local/lib/libCADPhysics.so /usr/local/lib/libcsread.so /home/annemarie/Downloads/hdf5-1.10.1/hdf5/lib/libhdf5_cpp.so /home/annemarie/Downloads/hdf5-1.10.1/hdf5/lib/libhdf5.so" SEM YieldCurve.gdml Yield${material}.mac > Yield${material}.log 2>&1
+	SEM YieldCurve.gdml Yield${material}.mac > Yield${material}.log 2>&1
 	echo Done with job ${job} for ${material}
 done
